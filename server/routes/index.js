e858f15c756c0eadb3ac08b01a7a5fbd6182fe44
MrXcao <francisco.ramos@gmail.com>
2024-01-06 19:58:27 -0300

    app

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bpoxTrap = require('../modules/boxTrap')
+const bpoxTrap = require('../modules/boxTrap');
 // eslint-disable-next-line no-unused-vars
 
-router.get('/', function(req, res) {	
+router.get('/', function(req, res) {
 	const data = {
+		app:'dogTools',
 		ok:true,
 	};
-	res.status(200).send(data);	
+	res.status(200).send(data);
 });
 router.post('/post', async (req, res) => {
 	if (req.body) {
@@ -19,11 +20,12 @@ router.post('/post', async (req, res) => {
 });
 router.post('/boxTrapText', async (req, resp) => {
 	try {
-		const res = await bpoxTrap.makeText(req.body.texto)
-		resp.send(res)
-	} catch (error) {
-		resp.status(500).send(error)
-	}	
+		const res = await bpoxTrap.makeText(req.body.texto);
+		resp.send(res);
+	}
+	catch (error) {
+		resp.status(500).send(error);
+	}
 });
 
 module.exports = router;
